5f4825c35263451820465b75a9858c66bca87fe0
Eric Lee <szl0094@auburn.edu>
2023-01-24 17:45:22 -0600

    spawning gun at runtime

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "ShooterCharacter.generated.h"
 
+class AGun;
+
 UCLASS()
 class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
 {
@@ -34,4 +36,11 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float RotationRate = 10;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AGun> GunClass;
+
+	UPROPERTY()
+	AGun* Gun;
+	
 };
