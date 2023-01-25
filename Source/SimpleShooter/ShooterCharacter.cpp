8c0d46e3e7e470936ae96fb1d801a582dd031a31
Eric Lee <szl0094@auburn.edu>
2023-01-24 18:19:12 -0600

    removed default and attached to meshes via socket

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -18,6 +18,10 @@ void AShooterCharacter::BeginPlay()
 	Super::BeginPlay();
 	
 	Gun = GetWorld()->SpawnActor<AGun>(GunClass);
+	GetMesh()->HideBoneByName(TEXT("weapon_r"), EPhysBodyOp::PBO_None);
+	Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, TEXT("WeaponSocket"));
+	Gun->SetOwner(this);
+
 }
 
 // Called every frame
