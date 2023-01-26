e16985c8910ed0ca6892182a2c980d1db739e246
Eric Lee <szl0094@auburn.edu>
2023-01-26 16:32:27 -0600

    blend anim by bool & bp pure nodes - pawn death

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -26,6 +26,11 @@ void AShooterCharacter::BeginPlay()
 
 }
 
+bool AShooterCharacter::IsDead() const
+{
+	return Health <= 0;
+}
+
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
