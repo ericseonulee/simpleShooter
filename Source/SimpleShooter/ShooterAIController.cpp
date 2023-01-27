f019134db7a20514596d8669e9d3a0e32ad88761
Eric Lee <szl0094@auburn.edu>
2023-01-27 17:02:26 -0600

    creating and setting up AI controller

---
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+    SetFocus(PlayerPawn);
+}
