e4ca7bcca61f41d0192b31aec01bc41aac03d440
Eric Lee <szl0094@auburn.edu>
2023-01-29 15:16:04 -0600

    custom BTTasks in C++

---
+++ Source/SimpleShooter/BTTask_ClearBlackBoardValue.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_ClearBlackBoardValue.h"
+
+
+UBTTask_ClearBlackBoardValue::UBTTask_ClearBlackBoardValue()
+{
+    NodeName = TEXT("Clear Blackboard Value");
+}
