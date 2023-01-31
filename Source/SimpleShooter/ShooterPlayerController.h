f24f322741824afbcbcdf2b6ff58b4a2dce2a0b8
Eric Lee <szl0094@auburn.edu>
2023-01-31 15:08:49 -0600

    crosshairs and HUDs

--- Source/SimpleShooter/ShooterPlayerController.h
+++ Source/SimpleShooter/ShooterPlayerController.h
@@ -17,7 +17,13 @@ class SIMPLESHOOTER_API AShooterPlayerController : public APlayerController
 public:
 	virtual void GameHasEnded(class AActor* EndGameFocus = nullptr, bool bIsWinner = false) override;
 
+protected:
+	virtual void BeginPlay() override;
+
 private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> HUDClass;
+	
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> WinScreenClass;
 
@@ -29,4 +35,5 @@ private:
 
 	FTimerHandle RestartTimer;
 	
+	UUserWidget* HUD;
 };
