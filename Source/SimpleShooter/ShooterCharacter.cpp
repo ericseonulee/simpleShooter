40ca202db82e8cc87e413ec2e5137c1f0b2942bf
Eric Lee <szl0094@auburn.edu>
2023-01-31 15:21:00 -0600

    Health bars

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -33,6 +33,11 @@ bool AShooterCharacter::IsDead() const
 	return Health <= 0;
 }
 
+float AShooterCharacter::GetHealthPercent() const
+{
+	return Health / MaxHealth;
+}
+
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
